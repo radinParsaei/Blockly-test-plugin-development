26eb3817df66e79d2905f34f0f1187cda10ecf17
radinParsaei <radinparsaei@gmail.com>
2021-09-20 23:32:27 +0430

    Some snippets have been added to the editor to make writing code easier (2)

--- Blockly-test-plugin-development.js
+++ Blockly-test-plugin-development.js
@@ -82,7 +82,18 @@ addMenuOption('import.svg', 'Debug Plugin', function() {
             {'name': 'is a plugin installed', 'code': 'Editor.isPluginInstalled(${0:pluginName})'},
             {'name': 'is dark', 'code': 'Editor.isDark()'},
             {'name': 'require a plugin', 'code': 'Editor.requirePlugin(${0:pluginName})'},
-            {'name': 'set blocks editor grid', 'code': "Editor.setBlocksEditorGrid({\n\tspacing : ${1:35},\n\tlength : ${2:9},\n\tcolor : ${3:'gray'},\n\tsnap : ${4:true}\n})"}])
+            {'name': 'set blocks editor grid', 'code': "Editor.setBlocksEditorGrid({\n\tspacing : ${1:35},\n\tlength : ${2:9},\n\tcolor : ${3:'gray'},\n\tsnap : ${4:true}\n})"},
+            {'name': 'set editor color', 'code': "Editor.setEditorColor(${0:'#f55'})"},
+            {'name': 'set theme', 'code': "Editor.setEditorColor(${0:'dark'})"},
+            {'name': 'set editor cancel color', 'code': "Editor.setEditorCancelColor(${0:'red'})"},
+            {'name': 'add button', 'code': "Editor.addButton(${1:'Logic'}, ${2:'show alert'}, ${3:function() {alert(\"Hello\")}})"},
+            {'name': 'get ace editor', 'code': "Editor.getAceEditor()"},
+            {'name': 'get Blockly', 'code': "Editor.getBlockly()"},
+            {'name': 'get Swal', 'code': "Editor.getSwal()"},
+            {'name': 'get code', 'code': "Editor.getCode()"},
+            {'name': 'is RTL', 'code': "Editor.isRTL"},
+            {'name': 'set direction (rtl, ltr)', 'code': "Editor.setBlocksEditorRTL(${0:false})"},
+            {'name': 'change theme(toggle)', 'code': "Editor.changeTheme()"}])
       m.snippetText = snippetText
       m.snippet = snippetManager.parseSnippetFile(snippetText, m.scope)
       snippetManager.register(m.snippet, m.scope)
